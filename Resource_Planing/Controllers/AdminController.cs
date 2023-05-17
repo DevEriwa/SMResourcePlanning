2bbd01b11b1728c278dfab381dabd795246bc46b
DevEriwa <freadjohnson1@gmail.com>
2023-05-17 11:23:57 +0100

    ControllersCreated

---
+++ Resource_Planing/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Resource_Planing.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Resource_Planing/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Resource_Planing.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
