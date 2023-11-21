26fb937ac6562836ba6f6dad60f9d93f30f2f2c5
Okechukwu-Mitchell <emperormitchell91@gmail.com>
2023-11-21 18:12:43 +0100

    leave date

--- Resource_Planing/Controllers/LeaveController.cs
+++ Resource_Planing/Controllers/LeaveController.cs
@@ -40,57 +40,123 @@ namespace Resource_Planing.Controllers
 
 
         [HttpPost]
-        public IActionResult StaffRequestLeave(string leaveDetails, string staffId)
-        {
-            if (leaveDetails != null)
-            {
-                var leaveViewModel = JsonConvert.DeserializeObject<RequestLeaveViewModel>(leaveDetails);
-                if (leaveViewModel != null)
-                {
-                    var userName = User.Identity.Name;
-                    ViewData["StaffLeave"] = GetRemainingLeave();
-                    var currentUser = _userHelper.FindByUserName(userName);
+		//public IActionResult StaffRequestLeave(string leaveDetails, string staffId)
+		//{
+		//    if (leaveDetails != null)
+		//    {
+		//        var leaveViewModel = JsonConvert.DeserializeObject<RequestLeaveViewModel>(leaveDetails);
+		//        if (leaveViewModel != null)
+		//        {
+		//            var userName = User.Identity.Name;
+		//            ViewData["StaffLeave"] = GetRemainingLeave();
+		//            var currentUser = _userHelper.FindByUserName(userName);
 
-                    if (currentUser != null)
-                    {
-                        var annualLeave = _userHelper.GetAnnualLeave(userName);
+		//            if (currentUser != null)
+		//            {
+		//                var annualLeave = _userHelper.GetAnnualLeave(userName);
 
-                        if (annualLeave == null)
-                        {
-                            return Json(new { isError = true, msg = "No Annual Leave created for your branch" });
-                        }
-                        else
-                        {
-                            if (Convert.ToDecimal(leaveViewModel.RemainingLeaveDays) < Convert.ToDecimal(leaveViewModel.NumberOfDaysRemaining))
-                            {
-                                return Json(new { isError = true, msg = "Remaining number of days for this leave has Exceeded!" });
-                            }
+		//                if (annualLeave == null)
+		//                {
+		//                    return Json(new { isError = true, msg = "No Annual Leave created for your branch" });
+		//                }
+		//                else
+		//                {
+		//                    if (Convert.ToDecimal(leaveViewModel.RemainingLeaveDays) < Convert.ToDecimal(leaveViewModel.NumberOfDaysRemaining))
+		//                    {
+		//                        return Json(new { isError = true, msg = "Remaining number of days for this leave has Exceeded!" });
+		//                    }
 
-                            if (leaveViewModel.LeaveTypeId == annualLeave.Id)
-                            {
-                                leaveViewModel.LeaveTypeName = "Annual Leave";
-                            }
+		//                    if (leaveViewModel.LeaveTypeId == annualLeave.Id)
+		//                    {
+		//                        leaveViewModel.LeaveTypeName = "Annual Leave";
+		//                    }
 
-                            var addleave = _leaveApplicationHelper.StaffRequestLeave(leaveViewModel, staffId);
+		//                    var addleave = _leaveApplicationHelper.StaffRequestLeave(leaveViewModel, staffId);
 
-                            if (addleave)
-                            {
-                                return Json(new { isError = false, msg = "Leave Added successfully" });
-                            }
+		//                    if (addleave)
+		//                    {
+		//                        return Json(new { isError = false, msg = "Leave Added successfully" });
+		//                    }
 
-                            return Json(new { isError = true, msg = "Unable To Add Leave" });
-                        }
-                    }
-                }
+		//                    return Json(new { isError = true, msg = "Unable To Add Leave" });
+		//                }
+		//            }
+		//        }
 
-                return Json(new { isError = true, msg = "Error Occurred" });
-            }
+		//        return Json(new { isError = true, msg = "Error Occurred" });
+		//    }
 
-            return Json(new { isError = true, msg = "Error Occurred" });
-        }
+		//    return Json(new { isError = true, msg = "Error Occurred" });
+		//}
+
+		public IActionResult StaffRequestLeave(string leaveDetails, string staffId)
+		{
+			try
+			{
+				if (string.IsNullOrEmpty(leaveDetails) || string.IsNullOrEmpty(staffId))
+				{
+					return BadRequest(new { isError = true, msg = "Invalid input parameters" });
+				}
+
+				var leaveViewModel = JsonConvert.DeserializeObject<RequestLeaveViewModel>(leaveDetails);
+
+				if (leaveViewModel == null)
+				{
+					return BadRequest(new { isError = true, msg = "Invalid leave details" });
+				}
+
+				var userName = User.Identity?.Name;
+
+				if (string.IsNullOrEmpty(userName))
+				{
+					return Unauthorized(new { isError = true, msg = "User not authenticated" });
+				}
+
+				ViewData["StaffLeave"] = GetRemainingLeave();
+				var currentUser = _userHelper.FindByUserName(userName);
+
+				if (currentUser == null)
+				{
+					return Json(new { isError = true, msg = "User not found" });
+				}
+
+				var annualLeave = _userHelper.GetAnnualLeave(userName);
+
+				if (annualLeave == null)
+				{
+					return Json(new { isError = true, msg = "No Annual Leave created for your branch" });
+				}
+
+				if (Convert.ToDecimal(leaveViewModel.RemainingLeaveDays) < Convert.ToDecimal(leaveViewModel.NumberOfDaysRemaining))
+				{
+					return Json(new { isError = true, msg = "Remaining number of days for this leave has exceeded" });
+				}
+
+				if (leaveViewModel.LeaveTypeId == annualLeave.Id)
+				{
+					leaveViewModel.LeaveTypeName = "Annual Leave";
+				}
+
+				var addleave = _leaveApplicationHelper.StaffRequestLeave(leaveViewModel, staffId);
+
+				if (addleave)
+				{
+					return Json(new { isError = false, msg = "Leave Added successfully" });
+				}
+
+				return Json(new { isError = true, msg = "Unable To Add Leave" });
+			}
+			catch (Exception ex)
+			{
+				// Log the exception for troubleshooting
+				// Consider returning a more detailed error message for debugging purposes
+				return StatusCode(500, new { isError = true, msg = "Internal server error" });
+			}
+		}
 
 
-        public decimal GetRemainingLeave()
+
+		public decimal GetRemainingLeave()
         {
             var staff = _userHelper.FindAdminByUserName(User.Identity.Name);
             if (staff != null)
